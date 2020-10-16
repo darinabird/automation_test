26f3ae5d03f0b729d586b370ad2cae9e9e5c3979
Darina Bird <darinabird46722@gmail.com>
2020-10-16 17:25:50 +0300

    added explicit wait, selector with By

--- part3/lesson6_step3.py
+++ part3/lesson6_step3.py
@@ -4,7 +4,7 @@ import time
 import math
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope="function")
@@ -21,15 +21,15 @@ class TestAnswer:
     def test_diff_links(self, browser, num):
         link = f"https://stepik.org/lesson/{num}/step/1"
         browser.get(link)
-        time.sleep(5)
         answer = math.log(int(time.time()))
 
-        # обратить внимание на эту часть:)
-        blank = browser.find_element_by_css_selector('textarea[placeholder="Type your answer here..."]')
-        blank.sendkeys(answer)
+        blank = WebDriverWait(browser, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, ".quiz-component.ember-view")))
+        blank.sendkeys(answer.text)
 
-        button = WebDriverWait(browser, 5).until(ec.element_to_be_clickable((By.ID, "submit-button"))
-                                                 )
+        time.sleep(2)
+
+        button = WebDriverWait(browser, 5).until(ec.element_to_be_clickable((By.ID, "submit-button")))
         button.click()
 
         message = browser.find_element_by_id("verify_message")
